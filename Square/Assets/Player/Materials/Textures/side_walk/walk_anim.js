f15a34ce956aa8ffea2393ee448804db85a2c2de
Frederik Rabøl <frederik-rm@hotmail.com>
2012-10-26 21:12:57 +0200

    updated project
    added walk & anim pictures

---
+++ Square/Assets/Player/Materials/Textures/side_walk/walk_anim.js
@@ -0,0 +1,8 @@
+var Walk : Texture2D[];
+var framesPerSecond = 10.0;
+
+function Update () {
+    var index : int = Time.time * framesPerSecond;
+    index = index % Walk.Length;
+    renderer.material.mainTexture = frames[index];
+}
